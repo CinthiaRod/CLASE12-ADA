e7d434c3688ce34f128b3301d10a4fa95978c815
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-30 18:32:01 -0800

    Descripción del ejercicio

---
+++ Ejercicio1.js
@@ -0,0 +1,4 @@
+//Ejercicio 1: While
+//Crea un programa que solicite al usuario ingresar números 
+//continuamente hasta que el usuario ingrese un número negativo.
+// Luego, imprime la suma de todos los números ingresados.
