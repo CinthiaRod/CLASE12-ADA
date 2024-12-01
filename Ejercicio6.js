ac6ad3c00327ab8f9dc46db57839712a27c21f3f
CinthiaRod <Cinthiarb28@gmail.com>
2024-11-30 18:42:14 -0800

    Descripción del ejercicio

---
+++ Ejercicio6.js
@@ -0,0 +1,4 @@
+//Ejercicio 6: Iguales a 10 pero menores de 1000
+//Dada una matriz, recorrer sus valores y
+//sumar solo los números que estén por encima o sean iguales a 10,
+// pero menores que 1000.
